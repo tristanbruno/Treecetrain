34e11f1f6724b6957cc7df4ac469d4b88396e6f4
tristanbruno <tristanbrunoedu@gmail.com>
2026-05-02 10:58:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const CACHE = ‘trainlog-v1’;
-const ASSETS = [’/’, ‘/index.html’];
+const CACHE = 'tct-v1';
+const BASE = '/treecetrain/';
 
-self.addEventListener(‘install’, e => {
-e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([BASE, BASE + 'index.html']))
+  );
 });
 
-self.addEventListener(‘fetch’, e => {
-e.respondWith(
-caches.match(e.request).then(r => r || fetch(e.request))
-);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
