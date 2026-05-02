4d424c5180652d5c1116d35eea8712aedfda307c
tristanbruno <tristanbrunoedu@gmail.com>
2026-05-02 11:09:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,2 @@
-const CACHE = 'tct-v1';
-const BASE = '/Treecetrain/';
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([BASE, BASE + 'index.html']))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+// Service worker désactivé
+self.addEventListener(‘fetch’, e => {});
